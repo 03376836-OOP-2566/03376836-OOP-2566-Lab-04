cd462721dd321bbfd37254080bcfd69c3bdd44dc
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 15:55:54 +0700

    03376836-OOP-2566-Lab-04

---
+++ 0289/Lab04_Ex03/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int var1 = 10;
+int var2 = 20, var3 = 33, var4, var5 = -55;
+string var6 = "Hello World", float var7 = 10.7, long var8 = 1e8;
+
+System.Console.WriteLine(var1);
+System.Console.WriteLine(var2);
+System.Console.WriteLine(var3);
+System.Console.WriteLine(var4);
+System.Console.WriteLine(var5);
+System.Console.WriteLine(var6);
+System.Console.WriteLine(var7);
+System.Console.WriteLine(var8);
