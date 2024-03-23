363320b776135af4756f5d55246e71aace70d878
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 14:52:58 +0700

    commit Ex04

--- 65030179_Lab04/Lab04_Ex04/Program.cs
+++ 65030179_Lab04/Lab04_Ex04/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int var1 = 10;
+int var2 = var1;
+var var3 = var1 + var2;
+
+System.Console.WriteLine($"Value of var1 = {var1}");
+System.Console.WriteLine($"Value of var2 = {var2}");
+System.Console.WriteLine($"Value of var3 = {var3}");
+System.Console.WriteLine($"Type of var1 = {var1.GetType()}");
+System.Console.WriteLine($"Type of var2 = {var2.GetType()}");
+System.Console.WriteLine($"Type of var3 = {var3.GetType()}");
