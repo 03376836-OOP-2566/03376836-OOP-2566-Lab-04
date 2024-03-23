21db5554543fa8ccdc8289e447d99bb1ff3c4ecd
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 14:18:42 +0700

    commit Ex1

--- 65030179_Lab04/Lab04_Ex01/Program.cs
+++ 65030179_Lab04/Lab04_Ex01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int var1;
+int var2;
+float var3;
+string var4;
+
+System.Console.WriteLine(var1);
+System.Console.WriteLine(var2);
+System.Console.WriteLine(var3);
+System.Console.WriteLine(var4);
